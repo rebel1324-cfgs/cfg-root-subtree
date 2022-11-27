6c9ae2daf9bac2dcd2d81d686408a92a08251f96
rebel1324 <rebel1324@gmail.com>
2022-11-27 14:42:17 +0900

    Feature: keybind sync and change subtree info

--- manage.py
+++ manage.py
@@ -31,16 +31,21 @@ def get_jetbrain_keymaps():
 import xml.etree.ElementTree as ElementTree
 import itertools
 import copy
+import logging
 def sync_keybinds():
+    logging.info("keybind:0000:KEYBIND_START")
     common_keymaps = dict()
-    rtree = ElementTree.parse(".resources/common-keymaps.xml")
+    common_keybind_path = ".resources/common-keymaps.xml"
+    rtree = ElementTree.parse(common_keybind_path)
+    logging.info(f"keybind:0002:KEYBIND_PARSED:{common_keybind_path}")
+
     for a in rtree.findall("action"):
         common_keymaps[ a.attrib.get('id')  ] = a
 
+    logging.info(f"keybind:0003:KEYBIND_KEYLEN:{len(common_keymaps.keys())}")
+
     for iter in get_jetbrain_keymaps():
-        print("-------------------------")
-        print(iter)
-        print("-------------------------")
+        logging.info(f"keybind:0004:KEYBIND_OPEN:{iter}")
         etree = ElementTree.parse(iter)
 
         # copy the action id keys from the common keymaps
@@ -59,14 +64,20 @@ def sync_keybinds():
                 # deepcopy the element to prevent any kind of interferences
                 root[index] = element
                 action_sets.remove(action_id)
+                logging.info(f"keybind:0005:KEYBIND_REPLACE:{action_id}")
             index = index + 1
 
         for action_id in action_sets:
             element = copy.deepcopy( common_keymaps[action_id])
             root.append(element)
+            logging.info(f"keybind:0006:KEYBIND_APPEND:{action_id}")
 
-        print(root)
+        logging.info(f"keybind:0007:KEYBIND_WRITE:{iter}")
+        etree.write(iter)
+
+    logging.info("keybind:0001:KEYBIND_END")
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     # todo: cli-fy
     sync_keybinds()
